0f9f20ed91e2b11bfef3c007a90d63d3ee560cc8
Matt Allan <matt@destructure.co>
2024-08-05 23:58:19 -0400

    cleanup

--- waves.c
+++ waves.c
@@ -49,52 +49,38 @@ inline void apu_enable(void)
 	NR50_REG = 0x77; // max volume L&R
 }
 
-// TODO: replace most of these with more specific fns
+inline void pu1_set_sweep(uint8_t pace, enum sweep_dir dir, uint8_t step)
+{
+	struct sweep sweep = PU1.sweep;
+	sweep.pace = pace;
+	sweep.dir = dir;
+	sweep.step = step;
 
-// inline void pu1_sweep_set(uint8_t pace, enum sweep_dir dir, uint8_t step)
-// {
-// 	assert(pace <= 7);
-// 	assert(step <= 7);
-// 	NR10_REG =  (pace << 4) | (dir << 3) | step;
-// }
+	NR10_REG =  (pace << 4) | (dir << 3) | step;
+}
 
-// inline void pu1_sweep_pace_set(uint8_t pace)
-// {
-// 	assert(pace <= 7);
-// 	NR10_REG =  (pace << 4) | (NR10_REG & 0xF);
-// }
 
-// inline void pu1_sweep_dir_set(enum sweep_dir dir)
-// {
-// 	NR10_REG =  (dir << 3) | (NR10_REG & 0xF7);
-// }
+inline void pu1_set_duty_cycle(enum duty_cycle duty)
+{
+	PU1.duty_cycle = duty;
+	NR11_REG = (duty << 6) | (NR11_REG & 0x1F);
+}
 
-// inline void pu1_sweep_step_set(uint8_t step)
-// {
-// 	assert(step <= 7);
-// 	NR10_REG =  step | (NR10_REG & 0xF8);
-// }
-
-// inline void pu1_duty_cycle_set(enum duty_cycle duty)
-// {
-// 	NR11_REG = (duty << 6) | (NR11_REG & 0x1F);
-// }
-
-// inline void pu1_length_counter_set(uint8_t counter)
-// {
-// 	assert(counter <= 63);
-// 	pu1_length_enabled = counter > 0;
-// 	NR11_REG = counter | (NR11_REG & 0xC0);
-// }
+inline void pu1_set_length(uint8_t len)
+{
+	PU1.length = len;
+	NR11_REG = len | (NR11_REG & 0xC0);
+}
 
 inline void pu1_set_env(uint8_t env_val)
 {
 	NR12_REG = env_val;
 }
 
-inline void pu1_trigger(uint16_t period)
+inline void pu1_trigger()
 {
-	uint8_t len_en = 0;
+	uint8_t len_en = PU1.length != 0;
+	uint16_t period = PU1.period;
 
 	NR13_REG = period & 0xFF;
 	NR14_REG = (1 << 7) | (len_en << 6) | (period >> 8);
@@ -105,7 +91,7 @@ void tim(void)
 	uint8_t env_val = envelope_tick(&PU1.envelope);
 	if (env_val != 0) {
 		pu1_set_env(env_val);
-		pu1_trigger(710); // TODO: less hacky way to set this
+		pu1_trigger();
 	}
 }
 
@@ -120,8 +106,7 @@ void main(void)
 	set_interrupts(VBL_IFLAG | TIM_IFLAG);
 	enable_interrupts();
 
-	uint16_t period = 710;
-
+	PU1.period = 710;
 	PU1.envelope.attack = 7;
 	PU1.envelope.decay = 7;
 	PU1.envelope.sustain = 2;
@@ -132,14 +117,14 @@ void main(void)
 		if (key_ticked(J_A)) {
 			CRITICAL
 			{
-				NR12_REG = envelope_on(&PU1.envelope, MAX_VOLUME);
-				pu1_trigger(period);
+				pu1_set_env(envelope_on(&PU1.envelope, MAX_VOLUME));
+				pu1_trigger();
 			}
 		} else if (key_released(J_A)) {
 			CRITICAL
 			{
-				NR12_REG = envelope_off(&PU1.envelope);
-				pu1_trigger(period);
+				pu1_set_env(envelope_off(&PU1.envelope));
+				pu1_trigger();
 			}
 		}
 
